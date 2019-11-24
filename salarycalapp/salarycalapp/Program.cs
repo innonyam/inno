bcd185dcd8e7582a12d85bbcd2940a462fd598d2
innonyam <58148726+innonyam@users.noreply.github.com>
2019-11-25 00:41:57 +0200

    Add files via upload

---
+++ salarycalapp/salarycalapp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace salarycalapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         var Employee1=new SalaryCalculator( 1, "james", "section");
+                    Console.WriteLine(" your net salary is: " + SalaryCalculator.CalculateSalary(500000));
+                    Console.WriteLine(" your Hourly salary:"+SalaryCalculator.CalculateHourly(12, 34));
+                    Console.WriteLine("your Hourly salary:"+ SalaryCalculator.CalculateDaily(12));
+        }
+    }
+}
